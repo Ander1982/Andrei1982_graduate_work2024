4505ff9f9bdc10db35a1cf5ca57eea007411a3c9
Andrei1982 <pasevich.andrei@yandex.ru>
2024-02-27 15:57:47 +0200

    27/02

--- Diploma/car_service/telebot/sendmessage.py
+++ Diploma/car_service/telebot/sendmessage.py
@@ -9,17 +9,16 @@ def send_telegram(tg_name, tg_phone, tg_sms):
     text = settings.tg_message
     api = 'https://api.telegram.org/bot'
     method = api + token + '/sendMessage'
-#
-    if text.find('{') and text.find('}') and text.rfind('{'):
+    #
+    if text.find('{') and text.find('}') and text.rfind('{') and text.find('(') and text.find(')'):
         part_1 = text[:text.find('{')]
-        part_2 = text[:text.rfind('}')]
-        part_3 = text[text.find('}') + 1:text.rfind('{')]
+        part_2 = text[text.find('}') + 1:text.rfind('{')]
+        part_3 = text[text.rfind('}') + 1:text.rfind('(')]
 
         text_slice = part_1 + tg_name + part_2 + tg_phone + part_3 + tg_sms
-
     else:
         text_slice = text
-#
+    #
     req = requests.post(method, data={
         'chat_id': chat_id,
         'text': text_slice
